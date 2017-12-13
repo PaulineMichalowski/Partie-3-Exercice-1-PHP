dba0378f282379f4bf7133868747cd4b17c2c694
Pauline Michalowski <michalowski.pauline@hotmail.fr>
2017-12-13 10:11:54 +0100

    Création de l'exercice 1

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 1</title>
+  </head>
+  <body>
+    <?php
+      $number = 0;
+      while($number <= 10) {
+        echo ' ' . $number++ . ' ';
+      }
+    ?>
+  </body>
+</html>
